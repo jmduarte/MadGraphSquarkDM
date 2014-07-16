f388603d18262b508da3c999b9f83ee47faf7def
Javier <jduarte@caltech.edu>
2014-07-16 22:34:41 +0200

    for three samples

--- SMS_LHE_Production/merging_SquarkDM.py
+++ SMS_LHE_Production/merging_SquarkDM.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
                 if (mM >= mDM):
                     GammaMin = (mM / (16*rt.TMath.Pi()) ) * rt.TMath.Power(1.0 - rt.TMath.Power(mDM/mM,2.0),2.0)
                     for gM in [GammaMin, mM/100.0, mM/3.0]:
-                        if not glob.glob("%s/8TeV_%s_%i_%i_%f.lhe.gz"%(outdir,sample,mDM,mM,gM)):
-                            for fileName in glob.glob("%s/8TeV_%s_2j_%i_%i_%f_run*.lhe.gz"%(outdir,sample,mDM,mM,gM)):
+                        if not glob.glob("%s/8TeV_%s_%i_%i_%i.lhe.gz"%(outdir,sample,mDM,mM,gM)):
+                            for fileName in glob.glob("%s/8TeV_%s_2j_%i_%i_%i_run*.lhe.gz"%(outdir,sample,mDM,mM,gM)):
                                 fileName = fileName.replace(".gz\n","")
                                 print fileName
         

a0c6dbe42eb47b8cf2151809b32415948e4113f5
Javier Duarte <jduarte@caltech.edu>
2015-08-09 18:51:56 +0200

    BR

--- SMS_LHE_Production/merging_T2bH.py
+++ SMS_LHE_Production/merging_T2bH.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
             mNLSP = mLSP+130
             for BR in [1.0,0.5]:
                 if (mM > mNLSP):
-                        if not glob.glob('%s/8TeV_%s_%i_%i_run1_%ievnt.lhe.gz'%(outdir,sample,mM,mLSP,400000)):
+                        if not glob.glob('%s/8TeV_%s_%i_%i_BR%.1f_run1_%ievnt.lhe.gz'%(outdir,sample,mM,mLSP,BR,400000)):
                             print '%s/8TeV_%s_%i_%i_run*.lhe.gz'%(indir,"T2tt_2j",mM,mLSP)
                             for fileName in glob.glob('%s/8TeV_%s_%i_%i_run*.lhe.gz'%(indir,"T2tt_2j",mM,mLSP)):
                                 print 'copying %s'%fileName
@@ -29,19 +29,19 @@ if __name__ == '__main__':
                                 fileName = fileName.split('/')[-1]
                                 exec_me('gzip -d tmp_dir/%s'%fileName)
                                 fileName = fileName.replace('.gz','')
-                                exec_me('grep -c  "<event>" tmp_dir/%s >> 8TeV_%s_%i_%i.txt'%(fileName,sample,mM,mLSP))
+                                exec_me('grep -c  "<event>" tmp_dir/%s >> 8TeV_%s_%i_%i_BR%.1f.txt'%(fileName,sample,mM,mLSP,BR))
                                 exec_me('grep \'Integrated weight (pb)\' tmp_dir/%s >> loXsec.txt'%(fileName))
                             xsecFile = open('loXsec.txt')
                             xsecList = [xsecString.split(":")[-1] for xsecString in xsecFile.readlines()]
                             print xsecList
                             xsec = xsecList[-1].replace("\n","")
-                            eventFile = open('8TeV_%s_%i_%i.txt'%(sample,mM,mLSP))
+                            eventFile = open('8TeV_%s_%i_%i_BR%.1f.txt'%(sample,mM,mLSP,BR))
                             eventList = [int(eventString) for eventString in eventFile.readlines()]
                             print eventList
                             nEvents = sum(eventList)
                             print 'total events to merge is %i'%nEvents
                             print 'cross section is %s pb'%xsec
-                            exec_me('rm 8TeV_%s_%i_%i.txt'%(sample,mM,mLSP))
+                            exec_me('rm 8TeV_%s_%i_%i_BR%.1f.txt'%(sample,mM,mLSP,BR))
                             exec_me('rm loXsec.txt')
                             exec_me('sed -e \'s/MSBOTTOM/%i/g\' -e \'s/MLSP/%i/g\' -e \'s/MNLSP/%i/g\' -e \'s/BRANCHINGRATIO2/%f/g\' -e \'s/BRANCHINGRATIO1/%f/g\' %s > banner.txt\n'%(mM,mLSP,mNLSP,BR,1-BR,paramcard))
                             exec_me('/opt/rocks/bin/perl merge-pl tmp_dir/8TeV_%s_%i_%i_*.lhe  8TeV_%s_%i_%i_BR%.1f_run1_%ievnt.lhe.gz  banner.txt'%('T2tt_2j',mM,mLSP,sample,mM,mLSP,BR,nEvents))
@@ -56,9 +56,9 @@ if __name__ == '__main__':
                             qcut = 45
                             
                             exec_me('python  mgPostProcv2.py  -o temp2.lhe -j 5 -q %i -e 5 -s temp.lhe'%qcut)
-                            exec_me('mv temp2.lhe PostProcessed/8TeV_%s_%i_%i_%.1f_run1_%ievnt.lhe'%(sample,mM,mLSP,BR,nEvents))
-                            exec_me('gzip PostProcessed/8TeV_%s_%i_%i_%.1f_run1_%ievnt.lhe'%(sample,mM,mLSP,BR,nEvents))
-                            exec_me('mv PostProcessed/8TeV_%s_%i_%i_%.1f_run1_%ievnt.lhe.gz %s/'%(sample,mM,mLSP,BR,nEvents,outdir))
+                            exec_me('mv temp2.lhe PostProcessed/8TeV_%s_%i_%i_BR%.1f_run1_%ievnt.lhe'%(sample,mM,mLSP,BR,nEvents))
+                            exec_me('gzip PostProcessed/8TeV_%s_%i_%i_BR%.1f_run1_%ievnt.lhe'%(sample,mM,mLSP,BR,nEvents))
+                            exec_me('mv PostProcessed/8TeV_%s_%i_%i_BR%.1f_run1_%ievnt.lhe.gz %s/'%(sample,mM,mLSP,BR,nEvents,outdir))
                             # clean up
                             exec_me('rm *.lhe; rm tmp_dir/*.lhe')
                             
